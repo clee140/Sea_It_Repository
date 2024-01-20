6fec452a4a9e8542ad0c330b7657713b97034054
Aditya Bose <adityabosemail@gmail.com>
2024-01-20 12:46:48 -0500

    Auth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,11 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
 //    implementation("com.google.firebase:firebase-ml-vision-image-label-model:19.0.0")
+
+    //Firebase signin stuff
+    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
 }
 
 android {
