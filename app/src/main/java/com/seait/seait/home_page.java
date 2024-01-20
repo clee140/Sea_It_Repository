de120e842c5b534bf4f477a4fb673b286c69ce11
Christopher Lee <christopherhlee6@gmail.com>
2024-01-20 10:28:31 -0500

    Home page.

--- app/src/main/java/com/seait/seait/home_page.java
+++ app/src/main/java/com/seait/seait/home_page.java
@@ -1,8 +1,14 @@
 package com.seait.seait;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class home_page extends AppCompatActivity {
 
@@ -11,6 +17,23 @@ public class home_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.navigation_profile) {
+                    startActivity(new Intent(home_page.this, profile.class));
+                    return true;
+                }
 
+                if (item.getItemId() == R.id.navigation_scan) {
+                    startActivity(new Intent(home_page.this, scan.class));
+                    return true;
+                }
+
+                // Handle item selection logic here
+                return true; // Return true to consume the selection event
+            }
+        });
     }
 }
