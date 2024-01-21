0d655b0d07b4499f32e68accc213d20b8453ada6
xanmankey <86136010+xanmankey@users.noreply.github.com>
2024-01-20 23:30:11 -0500

    started on auth

---
+++ app/src/main/java/com/seait/seait/auth.java
@@ -0,0 +1,20 @@
+package com.seait.seait;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class auth extends AppCompatActivity {
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
